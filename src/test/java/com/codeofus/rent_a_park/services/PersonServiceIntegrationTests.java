632267ff23a6ef259c1ddeb814b7059b8c5bf8a6
Sandi Slonjsak <sandi.slonjsak@gmail.com>
2023-03-10 11:47:53 +0100

    fix tests to confirm that cache indeed works.

---
+++ src/test/java/com/codeofus/rent_a_park/services/PersonServiceIntegrationTests.java
@@ -0,0 +1,26 @@
+package com.codeofus.rent_a_park.services;
+
+import com.codeofus.rent_a_park.IntegrationTest;
+import com.codeofus.rent_a_park.repositories.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+public class PersonServiceIntegrationTests extends IntegrationTest {
+
+    @Autowired
+    private PersonService personService;
+
+    @MockBean
+    private PersonRepository personRepository;
+
+    @Test
+    public void verifyCacheIsReturnedAfterFirstCall() {
+        personService.getAll();
+        personService.getAll();
+        verify(personRepository, times(1)).findAll();
+    }
+}
