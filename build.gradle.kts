d194f0e0cdd466840602f2f0c4d19f67f35aa82d
Ivona Raguz <ivona.raguz13@gmail.com>
2023-03-13 15:56:00 +0100

    remove duplication in build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     implementation("org.mapstruct:mapstruct-processor:1.4.2.Final")
     implementation("org.springframework:spring-jdbc:6.0.4")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
