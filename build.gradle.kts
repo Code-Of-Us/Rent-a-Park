40f346bc7fb2103c81c50e6551d7e72121d1a9b4
Ivona Raguz <ivona.raguz13@gmail.com>
2023-03-22 23:39:43 +0100

    implement service discovery

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.7.9"
+    id("org.springframework.boot") version "2.6.0"
     id("io.spring.dependency-management") version "1.1.0"
 }
 
@@ -13,14 +13,16 @@ var jdbcVersion = "6.0.4"
 var lombokVersion = "1.18.22"
 var testContainersVersion = "1.17.6"
 var jupiterVersion = "5.9.0"
+val springCloudVersion = "2021.0.0"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://artifactory-oss.prod.netflix.net/artifactory/maven-oss-candidates") }
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.0.4")
     implementation("org.springframework.boot:spring-boot-starter-web-services")
     implementation("org.projectlombok:lombok:$lombokVersion")
     implementation("org.mapstruct:mapstruct:${mapstructVersion}Final")
@@ -28,6 +30,7 @@ dependencies {
     implementation("org.springframework:spring-jdbc:$jdbcVersion")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
@@ -35,10 +38,15 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterVersion")
 
-
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("org.mapstruct:mapstruct-processor:${mapstructVersion}Final")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
+    }
 }
 
 tasks.withType<Test> {
